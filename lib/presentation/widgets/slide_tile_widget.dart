5207524deb5b90f9eef602263fab3034976bf1a7
nukeolay <66204571+nukeolay@users.noreply.github.com>
2021-03-18 12:11:56 +0300

    переделываю Сусанина

--- lib/presentation/widgets/slide_tile_widget.dart
+++ lib/presentation/widgets/slide_tile_widget.dart
@@ -8,23 +8,30 @@ class SlideTile extends StatelessWidget {
   final double width;
   final double height;
 
-
   SlideTile({this.title, this.imagePath, this.instruction, this.width, this.height});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(20.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          SizedBox(height: 20),
-          Text("$title", textAlign: TextAlign.center, style: TextStyle(fontSize: height * 0.04, fontWeight: FontWeight.bold, color: Colors.white)),
-          SizedBox(height: 10.0),
-          Image.asset(imagePath, width: width * 0.8),
-          SizedBox(height: 10.0),
-          Text("$instruction", textAlign: TextAlign.center, style: TextStyle(fontSize: height * 0.027, color: Colors.white)),
-        ],
+      child: Expanded(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(height: 50),
+              Text(
+                "$title",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: height * 0.04, fontWeight: FontWeight.bold, color: Colors.white),
+              ),
+              SizedBox(height: 20.0),
+              Image.asset(imagePath, width: width * 0.9),
+              SizedBox(height: 20.0),
+              Text("$instruction", textAlign: TextAlign.center, style: TextStyle(fontSize: height * 0.027, color: Colors.white)),
+            ],
+          ),
+        ),
       ),
     );
   }
