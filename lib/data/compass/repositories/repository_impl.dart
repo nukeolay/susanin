f335dfc2a5f87a7fadd73f21ed1ba9ad0b5a4bdc
nukeolay <66204571+nukeolay@users.noreply.github.com>
2022-05-13 09:15:42 +0300

    updated compass repository

--- lib/data/compass/repositories/repository_impl.dart
+++ lib/data/compass/repositories/repository_impl.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:dartz/dartz.dart';
 import 'package:susanin/core/errors/failure.dart';
 import 'package:susanin/data/compass/platform/compass_platform.dart';
@@ -7,16 +9,30 @@ import 'package:susanin/domain/compass/repositories/repository.dart';
 class CompassRepositoryImpl implements CompassRepository {
   final CompassPlatform compass;
 
-  CompassRepositoryImpl(this.compass);
+  CompassRepositoryImpl(this.compass) {
+    _init();
+  }
+
+  final StreamController<Either<Failure, CompassEntity>> _streamController =
+      StreamController.broadcast();
 
   @override
-  Stream<Either<Failure, CompassEntity>> get compassStream async* {
-    try {
-      await for (final compassPlatform in compass.compassStream) {
-        yield Right(compassPlatform); // ! TODO await for replace with listen
-      }
-    } catch (error) {
-      yield Left(CompassFailure());
-    }
+  Stream<Either<Failure, CompassEntity>> get compassStream =>
+      _streamController.stream;
+
+  void _init() async {
+    compass.compassStream.listen((event) {
+      _streamController.add(Right(event));
+    }).onError((error) {
+      _streamController.add(Left(CompassFailure()));
+    });
+
+    // try {
+    //   await for (final compassPlatform in compass.compassStream) {
+    //     yield Right(compassPlatform); // ! TODO await for replace with listen
+    //   }
+    // } catch (error) {
+    //   yield Left(CompassFailure());
+    // }
   }
 }
