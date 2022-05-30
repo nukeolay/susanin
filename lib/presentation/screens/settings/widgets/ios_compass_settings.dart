55b30532d41015063f13f8b7c45874ab3ac85b15
nukeolay <66204571+nukeolay@users.noreply.github.com>
2022-05-30 23:37:50 +0300

    ios compass calibration info updated

--- lib/presentation/screens/settings/widgets/ios_compass_settings.dart
+++ lib/presentation/screens/settings/widgets/ios_compass_settings.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:app_settings/app_settings.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'package:susanin/presentation/screens/settings/widgets/settings_button.dart';
 
 class IosCompassSettings extends StatelessWidget {
   const IosCompassSettings({Key? key}) : super(key: key);
@@ -23,67 +21,6 @@ class IosCompassSettings extends StatelessWidget {
             textAlign: TextAlign.left,
             style: const TextStyle(fontSize: 16),
           ),
-        ),
-        SettingsButton( // ! TODO choosee one button
-            text: '1 Перейти в настройки',
-            action: AppSettings.openAccessibilitySettings),
-        SettingsButton(
-            text: '2 Перейти в настройки', action: AppSettings.openAppSettings),
-        SettingsButton(
-            text: '3 Перейти в настройки',
-            action: AppSettings.openBatteryOptimizationSettings),
-        SettingsButton(
-            text: '4 Перейти в настройки',
-            action: AppSettings.openBluetoothSettings),
-        SettingsButton(
-            text: '5 Перейти в настройки',
-            action: AppSettings.openDataRoamingSettings),
-        SettingsButton(
-            text: '6 Перейти в настройки',
-            action: AppSettings.openDateSettings),
-        SettingsButton(
-            text: '7 Перейти в настройки',
-            action: AppSettings.openDevelopmentSettings),
-        SettingsButton(
-            text: '8 Перейти в настройки',
-            action: AppSettings.openDeviceSettings),
-        SettingsButton(
-            text: '9 Перейти в настройки',
-            action: AppSettings.openDisplaySettings),
-        SettingsButton(
-            text: '10 Перейти в настройки',
-            action: AppSettings.openHotspotSettings),
-        SettingsButton(
-            text: '11 Перейти в настройки',
-            action: AppSettings.openInternalStorageSettings),
-        SettingsButton(
-            text: '12 Перейти в настройки',
-            action: AppSettings.openLocationSettings),
-        SettingsButton(
-            text: '13 Перейти в настройки',
-            action: AppSettings.openLockAndPasswordSettings),
-        SettingsButton(
-            text: '14 Перейти в настройки',
-            action: AppSettings.openNFCSettings),
-        SettingsButton(
-            text: '15 Перейти в настройки',
-            action: AppSettings.openNotificationSettings),
-        SettingsButton(
-            text: '16 Перейти в настройки',
-            action: AppSettings.openSecuritySettings),
-        SettingsButton(
-            text: '17 Перейти в настройки',
-            action: AppSettings.openSoundSettings),
-        SettingsButton(
-            text: '18 Перейти в настройки',
-            action: AppSettings.openVPNSettings),
-        SettingsButton(
-          text: '19 Перейти в настройки',
-          action: AppSettings.openWIFISettings,
-        ),
-        SettingsButton(
-          text: '20 Перейти в настройки',
-          action: AppSettings.openWirelessSettings,
         ),
       ],
     );
