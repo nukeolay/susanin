d4c289de3308c1f800b2cd30cf5edb44909c951d
nukeolay <66204571+nukeolay@users.noreply.github.com>
2022-06-06 23:15:57 +0300

    added open maps and always-on on detailed screen

--- lib/presentation/screens/detailed_info/detailed_location_info_screen.dart
+++ lib/presentation/screens/detailed_info/detailed_location_info_screen.dart
@@ -1,10 +1,12 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:maps_launcher/maps_launcher.dart';
 import 'package:shimmer/shimmer.dart';
 import 'package:susanin/domain/location_points/entities/location_point.dart';
 import 'package:susanin/presentation/bloc/detailed_info_cubit/detailed_info_cubit.dart';
 import 'package:susanin/presentation/bloc/detailed_info_cubit/detailed_info_state.dart';
+import 'package:susanin/presentation/bloc/settings_cubit/settings_cubit.dart';
 import 'package:susanin/presentation/screens/common_widgets/hide_button.dart';
 import 'package:susanin/presentation/screens/detailed_info/widgets/location_details.dart';
 import 'package:susanin/presentation/screens/home/widgets/common/pointer.dart';
@@ -45,70 +47,97 @@ class DetailedInfoScreen extends StatelessWidget {
               child: CircularProgressIndicator(),
             );
           }
-
+          final isScreenAlwaysOn =
+              context.watch<SettingsCubit>().state.isScreenAlwaysOn;
           return Center(
             child: SingleChildScrollView(
               physics: const BouncingScrollPhysics(),
               child: Container(
                   alignment: Alignment.center,
                   padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      if (state.hasCompass)
-                        Pointer(
-                          rotateAngle: state.angle,
-                          arcRadius: state.pointerArc,
-                          positionAccuracy: state.positionAccuracy,
-                          radius: radius,
-                          foregroundColor:
-                              Theme.of(context).colorScheme.secondary,
-                          backGroundColor: Theme.of(context).cardColor,
-                        ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: Text(
-                          state.distance,
-                          style: const TextStyle(fontSize: 50),
-                        ),
-                      ),
-                      Row(
+                  child: Stack(
+                    children: [
+                      Column(
                         mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Flexible(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          if (state.hasCompass)
+                            Pointer(
+                              rotateAngle: state.angle,
+                              arcRadius: state.pointerArc,
+                              positionAccuracy: state.positionAccuracy,
+                              radius: radius,
+                              foregroundColor:
+                                  Theme.of(context).colorScheme.secondary,
+                              backGroundColor: Theme.of(context).cardColor,
+                            ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
                             child: Text(
-                              'accuracy_details'.tr(),
-                              textAlign: TextAlign.center,
-                              softWrap: true,
+                              state.distance,
+                              style: const TextStyle(fontSize: 50),
                             ),
                           ),
-                          Text(
-                            'distance_meters'.tr(args: [
-                              state.positionAccuracy
-                                  .toStringAsFixed(0)
-                                  .toString()
-                            ]),
-                            style: const TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Flexible(
+                                child: Text(
+                                  'accuracy_details'.tr(),
+                                  textAlign: TextAlign.center,
+                                  softWrap: true,
+                                ),
+                              ),
+                              Text(
+                                'distance_meters'.tr(args: [
+                                  state.positionAccuracy
+                                      .toStringAsFixed(0)
+                                      .toString()
+                                ]),
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 10.0),
+                          LocationDetails(
+                            pointName: state.locationName,
+                            pointLatitude:
+                                state.locationLatitude.toStringAsFixed(7),
+                            pointLongitude:
+                                state.locationLongitude.toStringAsFixed(7),
+                          ),
+                          const SizedBox(height: 10.0),
+                          LocationDetails(
+                            pointName: 'current_location'.tr(),
+                            pointLatitude:
+                                state.userLatitude.toStringAsFixed(7),
+                            pointLongitude:
+                                state.userLongitude.toStringAsFixed(7),
                           ),
                         ],
                       ),
-                      const SizedBox(height: 10.0),
-                      LocationDetails(
-                        pointName: state.locationName,
-                        pointLatitude:
-                            state.locationLatitude.toStringAsFixed(7),
-                        pointLongitude:
-                            state.locationLongitude.toStringAsFixed(7),
-                      ),
-                      const SizedBox(height: 10.0),
-                      LocationDetails(
-                        pointName: 'current_location'.tr(),
-                        pointLatitude: state.userLatitude.toStringAsFixed(7),
-                        pointLongitude: state.userLongitude.toStringAsFixed(7),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          IconButton(
+                              onPressed: () => context
+                                  .read<SettingsCubit>()
+                                  .toggleWakelock(),
+                              enableFeedback: true,
+                              tooltip: 'always_on_display'.tr(),
+                              icon: isScreenAlwaysOn
+                                  ? const Icon(Icons.lightbulb)
+                                  : const Icon(Icons.lightbulb_outline)),
+                          IconButton(
+                              onPressed: () => MapsLauncher.launchCoordinates(
+                                  state.locationLatitude,
+                                  state.locationLongitude,
+                                  state.locationName),
+                              icon: const Icon(Icons.map_rounded)),
+                        ],
                       ),
                     ],
                   )),
