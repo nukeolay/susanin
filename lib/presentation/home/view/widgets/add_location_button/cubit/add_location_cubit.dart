16bcbd0818510c2af209b97d0f8c58d75a506735
nukeolay <nukeolay@gmail.com>
2024-08-31 13:07:52 +0500

    add location cubit WIP

--- lib/presentation/home/view/widgets/add_location_button/cubit/add_location_cubit.dart
+++ lib/presentation/home/view/widgets/add_location_button/cubit/add_location_cubit.dart
@@ -4,6 +4,7 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:susanin/features/location/domain/entities/position.dart';
 import 'package:susanin/features/location/domain/repositories/location_repository.dart';
+import 'package:susanin/features/places/domain/entities/icon_entity.dart';
 import 'package:susanin/features/places/domain/entities/place_entity.dart';
 import 'package:susanin/features/places/domain/repositories/places_repository.dart';
 
@@ -65,13 +66,17 @@ class AddLocationCubit extends Cubit<AddLocationState> {
     );
   }
 
-  Future<void> onPressAdd(String pointName) async {
+  Future<void> onPressAdd({
+    required String pointName,
+    required IconEntity icon,
+  }) async {
     emit(state.copyWith(status: AddLocationStatus.loading));
     await _addLocation(
       latitude: state.latitude,
       longitude: state.longitude,
       notes: state.notes,
       name: _generateName(pointName),
+      icon: icon,
     );
   }
 
@@ -80,6 +85,7 @@ class AddLocationCubit extends Cubit<AddLocationState> {
     required String longitude,
     required String notes,
     required String name,
+    required IconEntity icon,
   }) async {
     emit(state.copyWith(status: AddLocationStatus.loading));
     final doubleLatitude = double.tryParse(latitude);
@@ -90,6 +96,7 @@ class AddLocationCubit extends Cubit<AddLocationState> {
       longitude: doubleLongitude,
       notes: notes,
       name: name,
+      icon: icon,
     );
   }
 
@@ -98,6 +105,7 @@ class AddLocationCubit extends Cubit<AddLocationState> {
     required double longitude,
     required String notes,
     required String name,
+    required IconEntity icon,
   }) async {
     try {
       final newPlace = PlaceEntity(
@@ -107,6 +115,7 @@ class AddLocationCubit extends Cubit<AddLocationState> {
         notes: notes,
         name: name,
         creationTime: DateTime.now(),
+        icon: icon,
       );
       _placesRepository.create(newPlace);
       emit(state.copyWith(status: AddLocationStatus.normal));
